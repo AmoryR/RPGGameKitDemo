590a9d317451c65a0e8b1af68ccb904e38736c42
Amory <theonlygeeking@gmail.com>
2021-07-04 20:44:04 +0200

    Create a hero and the ability to move it in the village

--- RPGGameKitDemo/Scenes/Village/VillageScene.swift
+++ RPGGameKitDemo/Scenes/Village/VillageScene.swift
@@ -5,8 +5,35 @@
 //  Created by Amory Rouault on 03/07/2021.
 //
 
+import SpriteKit
 import RPGGameKit
 
 class VillageScene: RPGGameScene {
+    
+    private var hero: RPGEntity!
+    
+    override func didMove(to view: SKView) {
+        
+        // Hero
+        self.hero = RPGEntity(textureName: "Hero")
+        self.hero.position.x = 16
+        self.hero.buildPhysics()
+        self.hero.add(to: self)
+        
+        // Camera
+        let camera = RPGCamera(scene: self)
+        self.hero.attach(camera: camera)
+        
+        // Movement
+        let heroGestureDetector = RPGGDEntity(entity: self.hero)
+        heroGestureDetector.addUI()
+        RPGGestureDetectorService.shared.register(heroGestureDetector, withKey: "HeroGestureDetector")
+        RPGGestureDetectorService.shared.activateGestureDetector(withKey: "HeroGestureDetector")
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        self.hero.update()
+    }
     
 }
